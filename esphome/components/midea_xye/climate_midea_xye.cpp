86791bd0cfaada71b500101403559439efdbf748
freewms <36444868+freewms@users.noreply.github.com>
2026-08-16 14:13:37 +0300

    Update climate_midea_xye.cpp

--- esphome/components/midea_xye/climate_midea_xye.cpp
+++ esphome/components/midea_xye/climate_midea_xye.cpp
@@ -135,12 +135,16 @@ void ClimateMideaXYE::sendRecv(uint8_t cmdSent) {
     // digitalWrite(ComControlPin, RS485_RX_PIN_VALUE);
 
     uint8_t i = 0;
+    uint8_t temp_byte;
     while (this->uart_->available()) {
       if (i < RX_MESSAGE_LENGTH) {
-        this->uart_->read_byte(&rx_data.raw[i]);
-        i++;
-      } else
-        this->uart_->flush();
+          this->uart_->read_byte(&rx_data.raw[i]);
+        } else {
+        // Если получили больше 32 байт - читаем и отбрасываем
+          this->uart_->read_byte(&temp_byte);
+          ESP_LOGV(Constants::TAG, "Dropping extra byte: 0x%02X", temp_byte);
+        }
+      i++;
     }
    
     if (i == RX_MESSAGE_LENGTH) {
